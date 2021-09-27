d8c5c6a136a0fb987b4aa1b8cd32b54add73444a
Emmanuel Egomson <fegomson@gmail.com>
2021-09-27 14:14:32 -0500

    Create Rating Component

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,21 +1,20 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import Rating from "../components/Rating"
 
 const Product = ({ product }) => {
 
   const {_id, name, image, price, rating, numReviews} = product
 
   return (
-    <Card className="my-3" rounded>
+    <Card className="my-3 rounded">
       <a href={`/product/${_id}`}>
         <Card.Img variant="top" src={image} />
       </a>
       <Card.Body>
         <Card.Title as="div">{name}</Card.Title>
         <Card.Text as="div">
-          <div className="my-3">
-            {rating} from {numReviews} reviews
-          </div>
+          <Rating value={rating} text={`from ${numReviews} reviews`} />
         </Card.Text>
         <Card.Text as="h3">
           ${price}
