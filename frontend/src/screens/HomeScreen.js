91818155044baf02644fb98f82c24a3ab922218a
Emmanuel Egomson <fegomson@gmail.com>
2021-09-28 17:59:43 -0500

    Create Message and Loader components

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -3,6 +3,8 @@ import {useDispatch, useSelector} from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product';
 import {listProducts} from "../actions/productActions"
+import Loader from "../components/Loader"
+import Message from '../components/Message';
 
 const HomeScreen = () => {
   const dispatch = useDispatch()
@@ -19,7 +21,7 @@ const HomeScreen = () => {
   return (
     <div>
       <h1>Latest Products</h1>
-      { loading ? <h2>Loading...</h2> : error ? <div>{error}</div> : <Row>
+      { loading ? <Loader/> : error ? <Message variant="danger">{error}</Message> : <Row>
         {products.map(product => (
           <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
             <Product product={product}/>
