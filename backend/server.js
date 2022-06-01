fcf10ec744a44c9bdbca8d49deaf5d0447509a27
Emmanuel Egomson <fegomson@gmail.com>
2022-06-01 14:52:54 -0500

    Sets up user authentication

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,32 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import connectDB from './config/db.js'
-import productRoutes from './routes/productRoutes.js'
-import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
-dotenv.config()
+dotenv.config();
 
-connectDB()
+connectDB();
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('API is running......');
-})
+app.use(express.json());
 
-app.use('/api/products', productRoutes)
+app.get("/", (req, res) => {
+  res.send("API is running......");
+});
+
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 // Fallback error for 404 not found
-app.use(notFound)
+app.use(notFound);
 
-// Error middleware 
-app.use(errorHandler)
+// Error middleware
+app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000
-const mode = process.env.NODE_ENV
+const PORT = process.env.PORT || 5000;
+const mode = process.env.NODE_ENV;
 
 app.listen(PORT, console.log(`Server running in ${mode} mode on port ${PORT}`));
